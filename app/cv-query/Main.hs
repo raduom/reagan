7bef6677ee815621117c3494374fae2b8ba1aae5
Radu Ometita <radu.ometita@gmail.com>
2019-06-16 10:04:56 +0300

    Add cv query program.

---
+++ app/cv-query/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Conduit
+import qualified Data.ByteString.Char8 as LC8
+
+import Reagan.Query.CV
+import Reagan.Query
+
+serialize :: ConstraintViolation -> String
+serialize (ConstraintViolation desc seed) =
+  seed ++ " : " ++ desc ++ "\n"
+
+main :: IO ()
+main = runConduitRes $
+     repositoryStream "." ["kcc_default"]
+  .| queryConstraintViolations
+  .| mapC (LC8.pack . serialize)
+  .| sinkFile "cv-query.out"
